cf2e096a0300473c511f03be3bccd65c26312178
The-Real-Anish <anishkul38@gmail.com>
2023-01-07 23:57:14 +0530

    Finished Thomson

--- Week2/Assignment-1/agents.py
+++ Week2/Assignment-1/agents.py
@@ -1,4 +1,5 @@
 from bandits import Bandit
+import numpy as np
 # Import libraries if you need them
 
 class Agent:
@@ -30,72 +31,116 @@ class Agent:
         return reward
 
 class GreedyAgent(Agent):
-    def __init__(self, bandits: Bandit, initialQ : float) -> None:
+
+    def __init__(self, bandits: Bandit, initialQ) -> None:
+        
         super().__init__(bandits)
+        assert(len(initialQ) == self.banditN)
         
-        # add any member variables you may require
+        #number of times each action is chosen, and initial Q values for all actions, are set
+        self.Q = initialQ
+        self.action_counter = np.zeros(self.banditN)
         
-    # implement
     def action(self) -> int:
-        pass
 
-    # implement
-    def update(self, choice: int, reward: int) -> None:
-        pass
+        return np.argmax(self.Q)
 
-class epsGreedyAgent(Agent):
-    def __init__(self, bandits: Bandit, epsilon : float) -> None:
-        super().__init__(bandits)
+    def update(self, action: int, reward: int) -> None:
+        assert 0 <= action < self.banditN
+        
+        self.action_counter[action] += 1
+        # - - - - - - - - This part below is our initial_1_k
+        self.Q[action] += (1/self.action_counter[action])(reward - self.Q[action])
+        return
+
+class epsGreedyAgent(GreedyAgent):
+
+    def __init__(self, bandits: Bandit, epsilon : float, initialQ) -> None:
+        
         self.epsilon = epsilon
-        # add any member variables you may require
+        super().__init__(bandits, initialQ)
     
-    # implement
     def action(self) -> int:
-        pass
 
-    # implement
-    def update(self, choice: int, reward: int) -> None:
-        pass
+        o_action = super().action()
+        #We have probabilities and shit now
+        prob = np.full(self.banditN, (self.epsilon / (self.banditN - 1)))
+        prob[o_action] = 1 - self.epsilon
 
-class UCBAAgent(Agent):
-    def __init__(self, bandits: Bandit, c: float) -> None:
-        super().__init__(bandits)
+        return np.argmax(np.random.multinomial(1, prob))
+
+    def update(self, action: int, reward: int) -> None:
+
+        return super().update(action, reward)
+
+class UCBAAgent(GreedyAgent):
+    
+    def __init__(self, bandits: Bandit, initialQ, c: float) -> None:
+        
         self.c = c
-        # add any member variables you may require
+        super().__init__(bandits, initialQ)
 
-    # implement
     def action(self) -> int:
-        pass
+        
+        if np.all(self.action_counter):
 
-    # implement
-    def update(self, choice: int, reward: int) -> None:
-        pass
+            return np.argmax(self.Q + self.c*np.sqrt(self.numiters/self.action_counter))
+        else:
+            #We'll try to try everything, till np.all evaluates to True
+            return np.argmin(self.action_counter)
+
+    def update(self, action: int, reward: int) -> None:
+        
+        return super().update(action, reward)
 
 class GradientBanditAgent(Agent):
-    def __init__(self, bandits: Bandit, alpha : float) -> None:
+
+    def __init__(self, bandits: Bandit, initial_1 : float, pref) -> None:
+        
         super().__init__(bandits)
-        self.alpha = alpha
-        # add any member variables you may require
+        self.initial_1 = initial_1
+        self.H = pref
 
-    # implement
     def action(self) -> int:
-        pass
+        
+        #policy determined using softmax distribution
+        return np.argmax( np.random.multinomial(1, np.exp(self.H)/ np.sum(np.exp(self.H)) ) )
 
-    # implement
-    def update(self, choice: int, reward: int) -> None:
-        pass
+    def update(self, action: int, reward: int) -> None:
+
+        self.H += self.initial_1*(reward - self.R) * (np.exp(self.H)/ np.sum(np.exp(self.H)) - np.eye(1, self.banditN, k=action)) 
+        self.R += (1/self.numiters) * (reward - self.R)
 
 class ThompsonSamplerAgent(Agent):
+
     def __init__(self, bandits: Bandit) -> None:
         super().__init__(bandits)
-        # add any member variables you may require
+        
+        if self.bandit.type == "Bernoulli":
+            self.initial_1 = np.full(self.banditN, 2.3)
+            self.initial_2 = np.full(self.banditN, 2.3)
 
-    # implement
+        else:
+            self.initial_1 = np.ones(self.banditN)
+            self.initial_2 = np.zeros(self.banditN)
+ 
     def action(self) -> int:
-        pass
+        
+        if self.bandit.type == "Bernoulli":
+            return np.argmax(np.random.beta(self.initial_1, self.initial_2))
+
+        else:
+            return np.argmax(np.random.normal(loc= self.initial_2, scale= self.first))
 
     # implement
-    def update(self, choice: int, reward: int) -> None:
-        pass
+    def update(self, action: int, reward: int) -> None:
+        
+        if self.bandit.type == "Bernoulli":
+            self.initial_1[action] += reward
+            self.initial_2[action] += 1 - reward
+
+        else:
+            self.initial_1[action] = self.initial_1[action]/np.sqrt(np.square(self.initial_1[action]) + 1)
+            self.initial_2[action] = (reward*np.square(self.initial_1[action]) + self.initial_2[action])/(np.square(self.initial_1[action]) + 1)
 
 # Implement other subclasses if you want to try other strategies
